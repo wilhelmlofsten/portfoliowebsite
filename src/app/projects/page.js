6990c805ad6dea46ee9fa074103e094df787d6f9
wilhelm <willo509@student.liu.se>
2024-12-07 22:14:27 +0100

    added shooting star background to projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,8 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { ShootingStars } from "../../components/ui/shooting-stars";
+import { StarsBackground } from "../../components/ui/start-background";
+
 export default function ProjectPage() {
+  // fixes the overflow issue when the page is loaded
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Projects Page</h1>
-      <p>This is the projects page.</p>
+    <div className="h-[60rem] md:h-[100vh] rounded-md bg-neutral-900 flex flex-col items-center justify-center relative w-full">
+      <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+        <span>Shooting Stars!</span>
+      </h2>
+      <ShootingStars />
+      <StarsBackground />
     </div>
   );
 }
