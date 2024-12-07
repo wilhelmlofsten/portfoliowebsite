752fffc604a850e09bb15e78493582a970b74d67
wilhelm <willo509@student.liu.se>
2024-12-07 17:57:11 +0100

    added beam background to contact page + fixed issue with scrolling

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { WavyBackground } from "../../components/ui/wavy-background";
+
 export default function ContactPage() {
+  //useEffect hook to disable scrolling on mount and enable scrolling
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>This is the contact page.</p>
+    <div className="w-full h-screen overflow-hidden">
+      <WavyBackground className="max-w-4xl mx-auto pb-40 w-full">
+        <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+          Heello 1
+        </p>
+        <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+          Hello 2
+        </p>
+      </WavyBackground>
     </div>
   );
 }
