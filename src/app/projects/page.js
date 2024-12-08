58d4053721c5d9427c5fb6aef17136831cd56bb7
wilhelm <willo509@student.liu.se>
2024-12-08 11:41:31 +0100

    added topics for expandable project cards and some projects

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -50,7 +50,7 @@ export default function ProjectPage() {
               <motion.div
                 layoutId={`card-${active.title}-${id}`}
                 ref={ref}
-                className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+                className="w-full max-w-[500px] h-full md:h-fit md:max-h-[90%] flex flex-col bg-gray-700 dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
               >
                 <motion.div layoutId={`image-${active.title}-${id}`}>
                   <Image
@@ -65,13 +65,13 @@ export default function ProjectPage() {
                 <div className="p-4">
                   <motion.h3
                     layoutId={`title-${active.title}-${id}`}
-                    className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
+                    className="font-medium text-white text-base"
                   >
                     {active.title}
                   </motion.h3>
                   <motion.p
                     layoutId={`description-${active.description}-${id}`}
-                    className="text-neutral-600 dark:text-neutral-400 text-base"
+                    className="text-gray-300 text-base"
                   >
                     {active.description}
                   </motion.p>
@@ -79,15 +79,30 @@ export default function ProjectPage() {
                     layout
                     href={active.ctaLink}
                     target="_blank"
-                    className="block mt-4 px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white text-center"
+                    className="block mt-4"
                   >
-                    {active.ctaText}
+                    <button className="p-[3px] relative group w-full">
+                      <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 rounded-lg transition duration-200 group-hover:from-blue-300 group-hover:to-blue-500" />
+                      <div className="px-8 py-2 bg-transparent rounded-[6px] relative text-base text-white">
+                        {active.ctaText}
+                      </div>
+                    </button>
                   </motion.a>
-                  <motion.div className="mt-4 text-neutral-600 text-sm dark:text-neutral-400">
+                  <motion.div className="mt-4 text-gray-300 text-sm">
                     {typeof active.content === "function"
                       ? active.content()
                       : active.content}
                   </motion.div>
+                  <div className="mt-4 flex flex-wrap gap-2">
+                    {active.tags.map((tag) => (
+                      <div
+                        key={tag}
+                        className="px-4 py-2 bg-gradient-to-r from-blue-400 to-blue-600 rounded-full text-white text-sm"
+                      >
+                        {tag}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </motion.div>
             </div>
@@ -114,13 +129,13 @@ export default function ProjectPage() {
                 <div className="flex justify-center items-center flex-col">
                   <motion.h3
                     layoutId={`title-${card.title}-${id}`}
-                    className="font-medium text-neutral-200 text-center md:text-left text-base"
+                    className="font-medium text-white text-center md:text-left text-base"
                   >
                     {card.title}
                   </motion.h3>
                   <motion.p
                     layoutId={`description-${card.description}-${id}`}
-                    className="text-neutral-400 text-center md:text-left text-base"
+                    className="text-gray-300 text-center md:text-left text-base"
                   >
                     {card.description}
                   </motion.p>
@@ -134,23 +149,55 @@ export default function ProjectPage() {
   );
 }
 
-// Cards Data
 const cards = [
   {
-    description: "Lana Del Rey",
-    title: "Summertime Sadness",
-    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaAKHFBCeaYsNP5YkIuS4pOzf6CakpWupPzg&s",
-    ctaText: "Visit",
-    ctaLink: "https://github.com/wilhelmlofsten",
-    content: () => <p>Some detailed description about Lana Del Rey.</p>,
+    description: "A social forum combined with the stock market",
+    title: "StockInsirator ",
+    src: "https://gitlab.liu.se/willo509/tddd27_2024_stockinspirator/-/raw/main/searchStockPage.png",
+    ctaText: "Visit the project",
+    ctaLink: "https://gitlab.liu.se/willo509/tddd27_2024_stockinspirator",
+    content: () => (
+      <p>
+        I developed a social platform, where users can also search, display and
+        follow stocks. The project was developed for the LiU course Advanced Web
+        programming (6 ECTS -TDDD27), and recieved the highest grade. It
+        features real-time stock data, news, and detailed statistics from
+        exchanges all around the world. Users can also search for other
+        investors and explore their portfolios. The application is built with
+        the MERN stack, utilizes Firebase for authentication and MongoDB for
+        data storage. Key tools include ChartJS, D3JS, for data visualisation
+        and Tailwind for styling.
+      </p>
+    ),
+    tags: [
+      "Fullstack",
+      "MongoDB",
+      "Express.js",
+      "React",
+      "Node.js",
+      "D3.js",
+      "Firebase",
+      "TailwindCSS",
+      "Finance",
+      "CI/CD",
+    ],
   },
   {
-    description: "Babbu Maan",
-    title: "Mitran Di Chhatri",
-    src: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    description: "This website!",
+    title: "Portfolio Website",
+    src: "https://gitlab.liu.se/willo509/tddd27_2024_stockinspirator/-/raw/main/searchStockPage.png",
     ctaText: "Visit",
-    ctaLink: "https://github.com/wilhelmlofsten",
+    ctaLink: "https://github.com/wilhelmlofsten/portfoliowebsite",
     content: () => <p>Some detailed description about Babbu Maan.</p>,
+    tags: [
+      "Fullstack",
+      "Next.js",
+      "TailwindCSS",
+      "React",
+      "AceternityUI",
+      "Github Pages",
+      "CI/CD",
+    ],
   },
   {
     description: "Metallica",
@@ -159,6 +206,7 @@ const cards = [
     ctaText: "Visit",
     ctaLink: "https://github.com/wilhelmlofsten",
     content: () => <p>Some detailed description about Metallica.</p>,
+    tags: ["Rock", "Band"],
   },
   {
     description: "Lord Himesh",
@@ -167,5 +215,6 @@ const cards = [
     ctaText: "Visit",
     ctaLink: "https://github.com/wilhelmlofsten",
     content: () => <p>Some detailed description about Himesh.</p>,
+    tags: ["Bollywood", "Singer"],
   },
 ];
